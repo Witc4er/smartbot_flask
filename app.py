4d4ff256e53b0cbfd93da827b288efdaa27ef698
witc4er.44 <witc4er.44@gmail.com>
2022-09-02 20:37:38 +0300

    Finished hw

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
+from datetime import datetime
+
 from flask import Flask, render_template, request, redirect
-from db.models import Note, Tag, db_session
+from db.models import Note, Tag, db_session, AddressBook, Email, Telephone
 
 app = Flask(__name__)
 app.debug = True
@@ -9,16 +11,49 @@ app.debug = True
 def index():
     return render_template("index.html")
 
+@app.route('/addressbook/', strict_slashes=False)
+def addressbook():
+    _addressbook = db_session.query(AddressBook).all()
+    return render_template('addressbook.html', addressbook=_addressbook)
+
+@app.route('/contact_create/', methods=['GET', 'POST'], strict_slashes=False)
+def contact_create():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        birthday = datetime.strptime(request.form.get('birthday'), '%Y-%m-%d').date()
+        address = request.form.get('address')
+        email = request.form.get('email')
+        phone = request.form.get('phone')
+        print(name, birthday, address, email, phone)
+        contact = AddressBook(name=name,
+                              birthday=birthday,
+                              address=address,
+                              email=[Email(email=email)],
+                              phone=[Telephone(phone=phone)])
+        db_session.add(contact)
+        db_session.commit()
+        return redirect('/addressbook/')
+    else:
+        pass
+    return render_template('contact_create.html')
+
+@app.route('/contact_detail/<id>', strict_slashes=False)
+def contact_detail(id):
+    contact = db_session.query(AddressBook).filter(AddressBook.id == id).first()
+    return render_template('contact_detail.html', contact=contact)
+
+@app.route('/contact_delete/<id>', strict_slashes=False)
+def contact_delete(id):
+    db_session.query(AddressBook).filter(AddressBook.id == id).delete()
+    db_session.commit()
+    return redirect('/addressbook/')
+
+
+
 @app.route('/notes/', strict_slashes=False)
 def notes():
     _notes = db_session.query(Note).all()
     return render_template('notes.html', notes=_notes)
-
-
-@app.route('/note_detail/<id>', strict_slashes=False)
-def detail(id):
-    note = db_session.query(Note).filter(Note.id == id).first()
-    return render_template('note_detail.html', note=note)
 
 
 @app.route('/note_create/', methods=['GET', 'POST'], strict_slashes=False)
@@ -26,7 +61,7 @@ def note_create():
     if request.method == 'POST':
         name = request.form.get('name')
         description = request.form.get('description')
-        tags = request.form.get('tags')
+        tags = request.form.getlist('tags')
         tags_obj = []
         for tag in tags:
             tags_obj.append(db_session.query(Tag).filter(Tag.name == tag).first())
@@ -38,6 +73,12 @@ def note_create():
         tags = db_session.query(Tag).all()
     return render_template('note_create.html', tags=tags)
 
+@app.route('/note_detail/<id>', strict_slashes=False)
+def note_detail(id):
+    note = db_session.query(Note).filter(Note.id == id).first()
+    return render_template('note_detail.html', note=note)
+
+
 
 @app.route('/tag/', methods=['GET', 'POST'], strict_slashes=False)
 def add_tag():
@@ -48,6 +89,20 @@ def add_tag():
         db_session.commit()
         return redirect('/notes/')
     return render_template('tag.html')
+
+@app.route('/done/<id>', strict_slashes=False)
+def done(id):
+    db_session.query(Note).filter(Note.id == id).first().done = True
+    db_session.commit()
+
+    return redirect('/notes/')
+
+@app.route('/delete/<id>', strict_slashes=False)
+def note_delete(id):
+    db_session.query(Note).filter(Note.id == id).delete()
+    db_session.commit()
+
+    return redirect('/notes/')
 
 if __name__ == '__main__':
     app.run()
--- db/models.py
+++ db/models.py
@@ -12,7 +12,7 @@ from db.db_conn import Base, engine, db_session
 note_m2m_tag = Table(
     "note_m2m_tag",
     Base.metadata,
-    Column("id", Integer, primary_key=True, autoincrement=True),
+    Column("id", Integer, primary_key=True),
     Column("note", Integer, ForeignKey("notes.id")),
     Column("tag", Integer, ForeignKey("tags.id")),
 )
@@ -25,7 +25,7 @@ class Note(Base):
     created = Column(DateTime, default=datetime.now())
     description = Column(String(150), nullable=False)
     done = Column(Boolean, default=False)
-    tags = relationship("Tag", secondary=note_m2m_tag, backref="notes")
+    tags = relationship("Tag", secondary="note_m2m_tag", backref="notes")
 
 
 class Tag(Base):
@@ -33,8 +33,28 @@ class Tag(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(25), nullable=False, unique=True)
 
+    def __init__(self, name):
+        self.name = name
+
     def __repr__(self) -> str:
         return self.name
+
+
+adb_m2m_tel = Table(
+    'adb_m2m_tel',
+    Base.metadata,
+    Column('id', Integer, primary_key=True, autoincrement=True),
+    Column('adb_id', Integer, ForeignKey('addressbook.id')),
+    Column('tel_id', Integer, ForeignKey('telephones.id'))
+)
+
+adb_m2m_email = Table(
+    'adb_m2m_email',
+    Base.metadata,
+    Column('id', Integer, primary_key=True, autoincrement=True),
+    Column('adb_id', Integer, ForeignKey('addressbook.id')),
+    Column('email_id', Integer, ForeignKey('emails.id'))
+)
 
 
 class AddressBook(Base):
@@ -43,10 +63,30 @@ class AddressBook(Base):
     name = Column(String, nullable=False)
     birthday = Column(Date, nullable=False)
     address = Column(String)
-    email = Column(String)
-    phone = Column(String)
+    email = relationship('Email', secondary='adb_m2m_email', backref='addressbook')
+    phone = relationship("Telephone", secondary='adb_m2m_tel', backref='addressbook')
     created_at = Column(DateTime, nullable=False, default=datetime.now())
     updated_at = Column(DateTime, nullable=False, default=datetime.now())
+
+
+class Telephone(Base):
+    __tablename__ = "telephones"
+    id = Column(Integer, nullable=False, primary_key=True, autoincrement=True)
+    phone = Column(String(25), nullable=False)
+    created_at = Column(DateTime, nullable=False, default=datetime.now())
+
+    def __repr__(self):
+        return f'<Telephone "{self.phone}">'
+
+
+class Email(Base):
+    __tablename__ = 'emails'
+    id = Column(Integer, nullable=False, primary_key=True, autoincrement=True)
+    email = Column(String(50), nullable=False)
+    created_at = Column(DateTime, nullable=False, default=datetime.now())
+
+    def __repr__(self):
+        return f'<Email "{self.email}">'
 
 
 if __name__ == "__main__":
